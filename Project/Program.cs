c473a1f2e260d7c2551ab5405aca65ae87385750
Nicholas Krijger <pcmanrules@gmail.com>
2020-06-23 14:26:29 +1200

    Initial

---
+++ Project/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool InternalGenerate = false;
+            List<object> TicketList = new List<object>();
+            ConsoleKeyInfo Option;
+            do
+            {
+                Console.Clear();
+                Console.Write("╔══════════════════════════════════════════════╗\n" +
+                              "║ Ticket System                                ║\n" +
+                              "╠══════════════════════════════════════════════╣\n" +
+                              "║ 1: Internal Ticket Generate                  ║\n" +
+                              "║ 2: Password Generator                        ║\n" +
+                              "║ Esc: Exit                                    ║\n" +
+                              "╚══════════════════════════════════════════════╝\n" +
+                              "Option: ");
+                Option = Console.ReadKey(false);
+                switch (Option.KeyChar.ToString())
+                {
+                    case "1":   //Internal Ticket Generate 
+                        Console.Clear();
+                        if (InternalGenerate == false)
+                        {
+                            TicketList.Add(new InternalTicket("Inna", "INNAM", "inna@whitecliffe.co.nz", "My monitor stopped working"));
+                            TicketList.Add(new InternalTicket("Maria", "MARIAH", "maria@whitecliffe.co.nz", "Request for a video camera to conduct webinar"));
+                            InternalGenerate = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Internal Ticket's have already been generated for this session");
+                        }
+                        Console.Write("Press any key to continue . . .");
+                        Console.ReadKey();
+                        break;
+                    case "2":   //Password Generator
+                        Console.Clear();
+                        PwdGen();
+                        Console.Write("Press any key to continue . . .");
+                        Console.ReadKey();
+                        break;
+                };
+            } while (Option.Key != ConsoleKey.Escape);
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+        static void PwdGen()
+        {
+            Console.WriteLine("Comming soon!");
+        }
+    }
+}
---
+++ Project/TicketCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Project
+{
+    public abstract class Ticket
+    {
+        private static int IDCounter = 0;
+        protected string CounterPlus()
+        {
+            IDCounter++;
+            return IDCounter.ToString();
+        }
+        protected string ID, Creator, StaffID, Email, Description, Response, Status;
+        public void StringOutput()
+        {
+            string Converted =
+                "Ticket Number: " + ID + "\n" +
+                "Ticket Creator: " + Creator + "\n" +
+                "Staff ID: " + StaffID + "\n" +
+                "Email Address: " + Email + "\n" +
+                "Description: " + Description + "\n" +
+                "Response: " + Response + "\n" +
+                "Ticket Status: " + Status;
+            Console.WriteLine(Converted);
+        }
+    }
+    public class InternalTicket : Ticket
+    {
+        public InternalTicket(string Creator, string StaffID, string Email, string Description)
+        {
+            this.ID = CounterPlus();
+            this.Creator = Creator;
+            this.StaffID = StaffID;
+            this.Email = Email;
+            this.Description = Description;
+            this.Status = "Open";
+        }
+    }
+}
